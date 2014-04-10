6b33acbfc08e3271bf151f98fcfe535d65b5d7d2
Ian O'Hara <ianohara@gmail.com>
2014-04-09 23:29:25 -0700

    Fix test CMakeLists and test vector set.

--- tests/vector.c
+++ tests/vector.c
@@ -6,11 +6,27 @@
 #include "test.h"
 
 /* Includes from the project source tree */
+#include "data_structures/vector.h"
+
 
 void test_vector__initialize(void) {
     global_test_counter++;
 }
 
 void test_vector__cleanup(void) {
+}
 
+void test_vector__set(void) {
+    v_t *v;
+    v = v_new(20);
+    v_data_t val = 2.0f;
+    cl_assert_(v, "Couldn't create a vector for setting.");
+
+    for (size_t i=0; i < v_len(v); i++)
+        cl_assert_equal_i_(v_set(v, i, val), E_OK, "Couldn't set.");
+
+    for (size_t i=0; i < v_len(v); i++)
+        cl_assert_(v_get(v,i) == val, "Get returned wrong value after setting.");
+
+    v_del(v);
 }
