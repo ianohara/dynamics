b6ab921301a4e4e8eab639dd8741e2c259a862d8
Ian O'Hara <ianohara@gmail.com>
2014-04-13 09:59:28 -0700

    Hashing out more of vector.

--- inc/data_structures/vector.h
+++ inc/data_structures/vector.h
@@ -27,23 +27,62 @@ typedef struct v {
 
 /* Returns a new vector of length len.  No values are set */
 v_t* v_new(size_t len);
-
-/* Returns a new vector with the values in *d filled into it */
-v_t* v_new_from_floats(float *d, size_t dlen);
-
 /* Deletes an existing vector and frees any memory it uses */
 error_t v_del(v_t *v);
 
+/****
+ * A number of helpful vector creating functions.  These all
+ * return a vector to a new vector that later needs to be
+ * freed with v_del by the caller.
+ *****/
+
+/* Returns a new vector with the values in *d filled into it */
+v_t* v_new_from_floats(float *d, size_t dlen);
+v_t* v_new_from_value(v_data_t val, size_t len);
+v_t* v_new_zeros(size_t len);
+v_t* v_new_ones(size_t len);
+
+/****
+ * Functions for modifying and getting properties of
+ * vectors.
+ ****/
 error_t v_set(const v_t *v, size_t ind, v_data_t val);
 v_data_t v_get(const v_t * const v, size_t ind);
 size_t v_len(const v_t * const v);
 
-/* Sum the two vs and put the result in res.  It is
-   fine for res to be the same as either the lhs or the rhs
+
+/****
+ * Functions for carrying out calculations on
+ * vectors.  For all of these, you are required
+ * to pass in an existing vector in which the result
+ * should be stored.  Only in specifically noted cases
+ * can this result vector pointer be the same as one
+ * of the operands.
+ ****/
+
+/* Multiplication by a scalar.
+
+    It is fine for res to be the same as v.
+ */
+error_t v_sp(v_data_t s, v_t *v, v_t *res);
+
+/* Sum the two vs and put the result in res.
+
+   It is fine for res to be the same as either the lhs or the rhs
 */
 error_t v_sum(v_t *lhs, v_t *rhs, v_t *res);
 
-/* Negate a v and put the result in res */
+/* Negate a v and put the result in res
+
+    It is fine for res to be the same as v.
+*/
 error_t v_negate(v_t *v, v_t *res);
+
+/* Return the dot product of lhs and rhs.
+ *
+ * Returns V_NAN if there was an error or if any
+ * of the values in the vectors to be dotted are V_NAN.
+ */
+v_data_t v_dot(v_t *lhs, v_t *rhs);
 
 #endif /* __VECTOR_H__23232323 */
--- src/data_structures/vector.c
+++ src/data_structures/vector.c
@@ -47,6 +47,28 @@ v_t* v_new_from_floats(float *d, size_t dlen)
     return v;
 }
 
+v_t* v_new_from_value(v_data_t val, size_t len)
+{
+    v_t *v = NULL;
+    if (!len) return NULL;
+
+    v = v_new(len);
+    if (!v) return NULL;
+
+    for (size_t i=0; i < v_len(v); i++)
+        v_set(v,i,val);
+
+    return v;
+}
+
+v_t* v_new_zeros(size_t len) {
+    return v_new_from_value((v_data_t)0.0, len);
+}
+
+v_t* v_new_ones(size_t len) {
+    return v_new_from_value((v_data_t)1.0, len);
+}
+
 error_t v_set(const v_t *v, size_t ind, v_data_t val) {
     if (!v) return E_NULLP;
     if (ind >= v->len) return E_VAL;
@@ -63,6 +85,17 @@ v_data_t v_get(const v_t * const v, size_t ind) {
 size_t v_len(const v_t * const v) {
     if (!v) return 0;
     return v->len;
+}
+
+error_t v_sp(v_data_t s, v_t *v, v_t *res)
+{
+    if (!v || !res) return E_NULLP;
+    if (v_len(v) != v_len(res)) return E_VAL;
+
+    for (size_t i=0; i < v_len(res); i++)
+        v_set(res,i,s*v_get(v,i));
+
+    return E_OK;
 }
 
 error_t v_sum(v_t *lhs, v_t *rhs, v_t *res) {
@@ -84,4 +117,15 @@ error_t v_negate(v_t *v, v_t *res) {
         v_set(res, i, -v_get(v,i));
 
     return E_OK;
+}
+
+v_data_t v_dot(v_t *lhs, v_t *rhs) {
+    v_data_t res = 0.0;
+    if (!lhs || !rhs) return V_NAN;
+    if (v_len(lhs) != v_len(rhs)) return V_NAN;
+
+    for (size_t i=0; i < v_len(lhs); i++)
+        res += v_get(lhs,i)*v_get(rhs,i);
+
+    return res;
 }
